b664c3bb82a15a7c4d6b07585bb79c4861e55b8e
A-cyberknight32 <adam.chodor.2012@gmail.com>
2024-09-28 13:05:05 +0200

    Update kodland.py

--- kodland.py
+++ kodland.py
@@ -1,3 +1,22 @@
-import random
-info = ["Lubię grać w piłkę","Mam 12 lat",'Chodzę do 6klasy']
-print(random.choice(info))
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+bot.run("MTI4NzAwMzY1NjIwOTU2Nzc2NA.GgFzD4.-RIbiL1r_zYmXO8jGH9BXcOZqFkkDEOUFfN6XI")
